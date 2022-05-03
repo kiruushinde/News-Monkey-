eea9c072f4d65ebcd0fca705ba08c3c2d5c4f559
Kiran Shinde <98176470+kiruushinde@users.noreply.github.com>
2022-05-03 10:55:20 +0530

    spinner

---
+++ newsapp/src/App.js
@@ -0,0 +1,19 @@
+
+import React, { Component } from 'react'
+import Navbar from './Components/Navbar'
+
+export default class App extends Component {
+
+  // name = "kiran" //this is class variable
+  // and we can access it by using this. --> method
+
+  render() {
+    return (
+      <div>
+        <Navbar />
+      </div>
+    )
+  }
+}
+
+
